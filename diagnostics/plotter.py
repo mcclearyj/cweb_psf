527ce67f675ddbea31044d05983016a0dea6b6b4
mcclearyj <jmac.ftw@gmail.com>
2023-02-06 00:16:28 -0500

    small param changes

--- diagnostics/plotter.py
+++ diagnostics/plotter.py
@@ -37,19 +37,17 @@ def set_rc_params():
 
     return
 
-def size_mag_plot(im_cat, star_cat, outdir, filter_name):
+def size_mag_plot(im_cat, star_cat, plot_name, filter_name):
     '''
     Save to file a size-magnitude plot with the stellar locus highlighted
     Inputs:
         im_cat: exposure catalog, either file name or Table() object
         star_cat : star catalog passed to PIFF, either file name or Table() object
-        outdir : directory in which to save plot_file_name
-        plot_file_name : plot file name
+        plot_name : plot file name
+        filter_name : filter name (no support for 30mas/60mas rn)
     '''
 
     set_rc_params()
-
-    plot_file_name = os.path.join(outdir, f'{filter_name}_sizemag.png')
 
     if type(im_cat) == str:
         image_catalog = Table.read(im_cat)
@@ -64,18 +62,18 @@ def size_mag_plot(im_cat, star_cat, outdir, filter_name):
     fig, ax = plt.subplots(1,1, tight_layout=True, figsize=(10,8))
 
     ax.plot(image_catalog['MAG_AUTO'], image_catalog['FWHM_WORLD']*3600, '.', \
-            label='all objects',markersize=2)
+            label='all objects', markersize=2)
     ax.plot(star_catalog['MAG_AUTO'], star_catalog['FWHM_WORLD']*3600, '*', \
-            label='selected stars',markersize=2)
+            label='selected stars', markersize=2)
 
     ax.set_xlabel('MAG_AUTO (zpt=30.0)', fontsize=14)
     ax.set_ylabel('FWHM_WORLD (arcsec)', fontsize=14)
-    ax.set_ylim(-0.05, 0.8)
-    ax.set_xlim(16,35)
+    ax.set_ylim(-0.05, 0.5)
+    ax.set_xlim(16, 35)
 
     ax.set_title(f'{str(filter_name)} SExtractor catalog', fontsize=14)
     ax.legend(markerscale=5, fontsize=14)
-    fig.savefig(plot_file_name)
+    fig.savefig(plot_name)
 
     return
 
